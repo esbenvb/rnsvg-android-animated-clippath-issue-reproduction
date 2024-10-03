7f29d73c2e98e0382849193784609d6dcd26b577
Esben von Buchwald <esben@vonbuchwald.dk>
2024-10-03 10:15:55 +0200

    Add code examples

--- App.tsx
+++ App.tsx
@@ -5,55 +5,195 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {
+  Animated,
+  Button,
+  Easing,
   SafeAreaView,
-  ScrollView,
   StatusBar,
-  StyleSheet,
   Text,
   useColorScheme,
   View,
 } from 'react-native';
+import {Circle, ClipPath, G, Mask, Rect, Svg} from 'react-native-svg';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const WIDTH = 300;
+const HEIGHT = 140;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const AnimatedRect = Animated.createAnimatedComponent(Rect);
+const AnimatedCircle = Animated.createAnimatedComponent(Circle);
+
+const SlidingInClipPath: React.FC = () => {
+  const animatedWidth = useRef(new Animated.Value(0)).current;
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    if (isVisible) {
+      Animated.timing(animatedWidth, {
+        toValue: WIDTH,
+        duration: 1200,
+        useNativeDriver: true,
+        easing: Easing.inOut(Easing.ease),
+      }).start();
+    } else {
+      animatedWidth.setValue(0);
+    }
+  }, [animatedWidth, isVisible]);
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      <Text>ClipPath sliding in</Text>
+      <Svg width={WIDTH} height={HEIGHT} fill={'blue'}>
+        <ClipPath id="clipPath">
+          <AnimatedRect x={0} y={0} width={animatedWidth} height={HEIGHT} />
+        </ClipPath>
+        <G clipPath="url(#clipPath)">
+          <Circle stroke={'green'} strokeWidth={3} cx={230} cy={50} r={30} />
+          <Rect x={50} y={30} width={200} height={100} />
+          <Circle stroke={'red'} strokeWidth={3} cx={80} cy={50} r={30} />
+        </G>
+      </Svg>
+      <Button
+        onPress={() => setIsVisible(current => !current)}
+        title={isVisible ? 'Hide' : 'SHow'}
+      />
     </View>
   );
-}
+};
+
+const SlidingInMask: React.FC = () => {
+  const animatedWidth = useRef(new Animated.Value(0)).current;
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    if (isVisible) {
+      Animated.timing(animatedWidth, {
+        toValue: WIDTH,
+        duration: 1200,
+        useNativeDriver: true,
+        easing: Easing.inOut(Easing.ease),
+      }).start();
+    } else {
+      animatedWidth.setValue(0);
+    }
+  }, [animatedWidth, isVisible]);
+  return (
+    <View>
+      <Text>Mask sliding in</Text>
+      <Svg width={WIDTH} height={HEIGHT} fill={'blue'}>
+        <Mask id="mask">
+          <Rect x={0} y={0} width={WIDTH} height={HEIGHT} fill="black" />
+          <AnimatedRect
+            x={0}
+            y={0}
+            width={animatedWidth}
+            height={HEIGHT}
+            fill="white"
+          />
+        </Mask>
+        <G mask="url(#mask)">
+          <Circle stroke={'green'} strokeWidth={3} cx={230} cy={50} r={30} />
+          <Rect x={50} y={30} width={200} height={100} />
+          <Circle stroke={'red'} strokeWidth={3} cx={80} cy={50} r={30} />
+        </G>
+      </Svg>
+      <Button
+        onPress={() => setIsVisible(current => !current)}
+        title={isVisible ? 'Hide' : 'SHow'}
+      />
+    </View>
+  );
+};
+
+const FadingIn: React.FC = () => {
+  const animatedOpacity = useRef(new Animated.Value(0)).current;
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    if (isVisible) {
+      Animated.timing(animatedOpacity, {
+        toValue: 1,
+        duration: 1200,
+        useNativeDriver: true,
+        easing: Easing.inOut(Easing.ease),
+      }).start();
+    } else {
+      animatedOpacity.setValue(0);
+    }
+  }, [animatedOpacity, isVisible]);
+  return (
+    <View>
+      <Text>Mask fading in</Text>
+      <Svg width={WIDTH} height={HEIGHT} fill={'blue'}>
+        <Mask id="mask">
+          <AnimatedRect
+            x={0}
+            y={0}
+            width={WIDTH}
+            opacity={animatedOpacity}
+            height={HEIGHT}
+            fill="white"
+          />
+        </Mask>
+        <G mask="url(#mask)">
+          <Circle stroke={'green'} strokeWidth={3} cx={230} cy={50} r={30} />
+          <Rect x={50} y={30} width={200} height={100} />
+          <Circle stroke={'red'} strokeWidth={3} cx={80} cy={50} r={30} />
+        </G>
+      </Svg>
+      <Button
+        onPress={() => setIsVisible(current => !current)}
+        title={isVisible ? 'Hide' : 'SHow'}
+      />
+    </View>
+  );
+};
+
+const RADIUS = 80;
+const PulsatingCircle: React.FC = () => {
+  const animatedRadius = useRef(new Animated.Value(0)).current;
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    if (isVisible) {
+      Animated.timing(animatedRadius, {
+        toValue: RADIUS,
+        duration: 1200,
+        useNativeDriver: true,
+        easing: Easing.inOut(Easing.ease),
+      }).start();
+    } else {
+      Animated.timing(animatedRadius, {
+        toValue: 0,
+        duration: 1200,
+        useNativeDriver: true,
+        easing: Easing.inOut(Easing.ease),
+      }).start();
+    }
+  }, [animatedRadius, isVisible]);
+  return (
+    <View>
+      <Text>Pulsating Circle</Text>
+      <Svg width={WIDTH} height={HEIGHT} fill={'blue'}>
+        <G>
+          <AnimatedCircle
+            fill={'green'}
+            strokeWidth={3}
+            cx={80}
+            cy={80}
+            r={animatedRadius}
+          />
+        </G>
+      </Svg>
+      <Button
+        onPress={() => setIsVisible(current => !current)}
+        title={isVisible ? 'Hide' : 'SHow'}
+      />
+    </View>
+  );
+};
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -68,51 +208,12 @@ function App(): React.JSX.Element {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <SlidingInClipPath />
+      <SlidingInMask />
+      <FadingIn />
+      <PulsatingCircle />
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
